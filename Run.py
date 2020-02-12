59d6e38e6c90319b64a4fa3973f0f8ccc559b04b
Karre Kumar (skarre) [CONT - Type 2 - XINDUS TECHNOLOGIES INC] <skarre@micron.com>
2020-02-12 11:28:19 +0530

    added get_run_id

--- Run.py
+++ Run.py
@@ -1,9 +1,17 @@
 def get_run_id(xindus_db_conn):
-  run_id = 2  # it has to be fetched from db, and whatever fetched from db + 1 has to be returned.
-  return run_id
+    mycursor = xindus_db_conn.cursor()
+    sql_read = "select MAX(RUN_ID) from RUN"
+    mycursor.execute(sql_read)
+    data = mycursor.fetchall()
+    print("Total number of rows is ", mycursor.rowcount)
+    for row in data:
+        run_id = row[0]
+        print(row[0])
+        print(row[1])
+    return run_id
 
-def Insert_Run_data():
-    mycursor = mydb.cursor()
+def insert_run_data(xindus_db_conn):
+    mycursor = xindus_db_conn.cursor()
     global START_DATE,START_TIME,END_DATE,END_TIME,Mode
     Mode='perf';
 
@@ -12,4 +20,4 @@ def Insert_Run_data():
         (START_DATE,START_TIME,END_DATE,END_TIME,Mode),
     ]
     mycursor.executemany(sql4,val4)
-    mydb.commit()
+    xindus_db_conn.commit()
