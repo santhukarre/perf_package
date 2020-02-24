fec03034de572a31eef299a7aa2a6289d2870df8
Karre Kumar (skarre) [CONT - Type 2 - XINDUS TECHNOLOGIES INC] <skarre@micron.com>
2020-02-24 19:53:53 +0530

    Adding python_module_insta..bat file.

---
+++ python_modules_install.bat
@@ -0,0 +1,16 @@
+echo off
+
+echo "installing xlsxwriter module"
+pip install xlsxwriter
+
+echo "installing mysql connector module"
+pip install mysql-connector-python
+
+echo "installing pandas module"
+pip install pandas
+
+echo "installing vincent module"
+pip install vincent
+
+echo "installing xlwt module"
+pip install xlwt
